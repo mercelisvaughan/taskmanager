85f5373ea38cbc2c67f8d0ccce878f9202ea6c61
mercelisvaughan <mercelisvaughan@gmail.com>
2021-11-05 21:01:33 -0400

    Initial file upload

---
+++ app/src/main/java/com/example/application/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.application;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onclick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+            }
+        });
+
+    }
+}
